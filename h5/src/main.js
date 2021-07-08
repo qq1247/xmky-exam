4b543cfc50248f05ceef4c2545efa1d26cb45e34
车 <184636408@qq.com>
2021-07-08 09:38:11 +0800

    mock数据的添加和文件格式化引起的变化

--- h5/src/main.js
+++ h5/src/main.js
@@ -3,15 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import '@/assets/style/normalize.css'
 import ElementUI from 'element-ui'
-import '@/assets/style/theme/index.css'
-import '@/assets/style/element-ui-ex.scss'
 import CKEditor from '@ckeditor/ckeditor5-vue2'
 import api from '@/api'
 import * as tools from '@/util/tools.js'
 Vue.use(ElementUI)
 Vue.use(CKEditor)
+
+import '@/assets/style/normalize.css'
+import '@/assets/style/theme/index.css'
+import '@/assets/style/element-ui-ex.scss'
 
 Vue.prototype.$https = api
 Vue.prototype.$tools = tools
