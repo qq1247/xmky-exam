f57d454880f002bd7acd9a2902fd859140a3bd63
车 <184636408@qq.com>
2021-08-13 15:03:36 +0800

    vuex模块化

---
+++ h5/src/store/modules/user.js
@@ -0,0 +1,120 @@
+/*
+ * @Description:
+ * @Version: 1.0
+ * @Company:
+ * @Author: Che
+ * @Date: 2021-08-11 11:33:30
+ * @LastEditors: Che
+ * @LastEditTime: 2021-08-13 13:25:19
+ */
+import { login } from '@/api/common'
+import {
+  getInfo,
+  setInfo,
+  removeInfo,
+  removeAsyncRoutes,
+  removeFinallyRoutes,
+} from '@/utils/storage'
+import router, { resetRouter } from '@/router/index'
+
+const state = {
+  token: getInfo().accessToken,
+  name: getInfo().userName,
+  userId: getInfo().userId,
+  roles: getInfo().roles,
+}
+
+const mutations = {
+  SET_TOKEN: (state, token) => {
+    state.token = token
+  },
+  SET_NAME: (state, name) => {
+    state.name = name
+  },
+  SET_USER_ID: (state, userId) => {
+    state.userId = userId
+  },
+  SET_ROLES: (state, roles) => {
+    state.roles = roles
+  },
+}
+
+const actions = {
+  /**
+   * @name: login
+   * @description: 登录
+   * @param {*} commit
+   * @param {*} userInfo
+   * @return {*}
+   */
+  login({ commit }, userInfo) {
+    const { username, password } = userInfo
+    return new Promise((resolve, reject) => {
+      login({ loginName: username.trim(), pwd: password })
+        .then((res) => {
+          const { data } = res
+          commit('SET_TOKEN', data.accessToken)
+          commit('SET_ROLES', data.roles)
+          commit('SET_NAME', data.userName)
+          commit('SET_USER_ID', data.userId)
+          setInfo(data)
+          resolve()
+        })
+        .catch((error) => {
+          reject(error)
+        })
+    })
+  },
+
+  /**
+   * @name: resetToken
+   * @description: 重置缓存信息
+   * @param {*} commit
+   * @return {*}
+   */
+  resetToken({ commit }) {
+    return new Promise((resolve) => {
+      commit('SET_TOKEN', '')
+      commit('SET_ROLES', [])
+      commit('SET_ROUTES', [])
+      removeInfo()
+      removeAsyncRoutes()
+      removeFinallyRoutes()
+      resetRouter()
+      resolve()
+    })
+  },
+
+  /**
+   * @name: changeRoles
+   * @description: 更新roles
+   * @param {*} commit
+   * @param {*} dispatch
+   * @param {*} role
+   * @return {*}
+   */
+  async changeRoles({ commit, dispatch }, role) {
+    const token = role + '-token'
+
+    commit('SET_TOKEN', token)
+    setToken(token)
+
+    const { roles } = await dispatch('getInfo')
+
+    resetRouter()
+
+    // generate accessible routes map based on roles
+    const accessRoutes = await dispatch('permission/generateRoutes', roles, {
+      root: true,
+    })
+    // dynamically add accessible routes
+    router.addRoutes(accessRoutes)
+  },
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+}
--- h5/src/store/state.js
+++
@@ -1,6 +0,0 @@
-const state = {
-  userInfo: localStorage.getItem('userInfo')
-    ? localStorage.getItem('userInfo')
-    : '',
-}
-export default state
