31ec724a515d437e376e0994429385aa6f813d7a
车 <184636408@qq.com>
2021-09-15 14:45:18 +0800

    feat：新增动态修改组织机构的名称和logo功能

--- h5/src/views/base/Index.vue
+++ h5/src/views/base/Index.vue
@@ -5,7 +5,7 @@
  * @Author: Che
  * @Date: 2021-07-27 17:31:01
  * @LastEditors: Che
- * @LastEditTime: 2021-08-16 10:27:18
+ * @LastEditTime: 2021-09-14 10:48:24
 -->
 <template>
   <div class="container">
@@ -16,22 +16,22 @@
       <div class="exam-list">
         <div class="exam-item">
           <div @click="toDict" class="exam-content exam-add">
-            <i class="common common-plus"></i>
+            <i class="common common-data-library"></i>
             <span>数据字典</span>
           </div>
         </div>
         <div class="exam-item">
           <div @click="toCron" class="exam-content exam-add">
-            <i class="common common-plus"></i>
+            <i class="common common-time"></i>
             <span>定时任务</span>
           </div>
         </div>
-        <!-- <div class="exam-item">
+        <div class="exam-item">
           <div @click="toParam" class="exam-content exam-add">
-            <i class="common common-plus"></i>
+            <i class="common common-setting"></i>
             <span>系统参数</span>
           </div>
-        </div>-->
+        </div>
       </div>
     </div>
   </div>
@@ -49,13 +49,18 @@ export default {
     toCron() {
       this.$router.push({ path: '/base/cron' })
     },
-    /* toParam() {
-      this.$router.push({path: "/base/param"})
-    } */
+    toParam() {
+      this.$router.push({ path: '/base/param' })
+    },
   },
 }
 </script>
 
 <style lang="scss" scoped>
 @import '../../assets/style/list-card.scss';
+.exam-add {
+  i {
+    font-size: 60px;
+  }
+}
 </style>
