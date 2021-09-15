8f468f97b09ec6df8a0502ef05085d0ee07da60d
车 <184636408@qq.com>
2021-09-15 14:48:51 +0800

    feat：新增编辑试题页详情的展示

---
+++ h5/src/components/QuestionDetail/DetailMain.vue
@@ -0,0 +1,54 @@
+<!--
+ * @Description: 
+ * @Version: 1.0
+ * @Company: 
+ * @Author: Che
+ * @Date: 2021-09-10 10:13:52
+ * @LastEditors: Che
+ * @LastEditTime: 2021-09-13 17:53:44
+-->
+<template>
+  <div class="detail-main">
+    <div class="detail-title" v-html="`${data.id}、${data.title}`"></div>
+    <template v-if="data.options && data.options.length > 0">
+      <div
+        class="detail-option"
+        v-for="(option, index) in data.options"
+        :key="index"
+        v-html="`${String.fromCharCode(65 + index)}、${option}`"
+      ></div>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      default: () => {},
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.detail-main {
+  margin-bottom: 40px;
+}
+.detail-title {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+}
+.detail-option {
+  display: flex;
+  padding-left: 30px;
+  line-height: 30px;
+  p,
+  span {
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ h5/src/components/QuestionDetail/Index.vue
@@ -0,0 +1,72 @@
+<!--
+ * @Description: 
+ * @Version: 1.0
+ * @Company: 
+ * @Author: Che
+ * @Date: 2021-09-10 10:12:59
+ * @LastEditors: Che
+ * @LastEditTime: 2021-09-13 14:55:25
+-->
+<template>
+  <div class="question-detail">
+    <DetailMain :data="data"></DetailMain>
+    <el-divider v-if="!moreStatus" @click="showMore"
+      >查看更多<i class="el-icon-arrow-down"></i
+    ></el-divider>
+    <DetailMore
+      v-if="moreStatus"
+      :data="data"
+      :type="type"
+      :difficulty="difficulty"
+    ></DetailMore>
+  </div>
+</template>
+
+<script>
+import DetailMore from './DetailMore.vue'
+import DetailMain from './DetailMain.vue'
+export default {
+  components: {
+    DetailMain,
+    DetailMore,
+  },
+  props: {
+    data: {
+      type: Object,
+      default: () => {},
+    },
+    type: {
+      type: Array,
+      default: [],
+    },
+    difficulty: {
+      type: Array,
+      default: [],
+    },
+  },
+  watch: {
+    data: {
+      deep: true,
+      handler() {
+        this.moreStatus = false
+      },
+    },
+  },
+  data() {
+    return {
+      moreStatus: false,
+    }
+  },
+  methods: {
+    showMore() {
+      this.moreStatus = !this.moreStatus
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.question-detail {
+  padding: 20px;
+}
+</style>
