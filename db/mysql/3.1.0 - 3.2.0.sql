aef664fc8ab2ec7af168ebcc7b71d489cd0a0c2f
qq1247 <1247qq@163.com>
2021-10-14 16:49:10 +0800

    启动自动备份数据库功能（定时任务+配置文件读取账号密码+批处理脚本+相对绝对路径情况保存不同位置）

---
+++ db/mysql/3.1.0 - 3.2.0.sql
@@ -0,0 +1,3 @@
+INSERT INTO `sys_cron` VALUES (2, '定时备份', 'com.wcpdoc.exam.quartz.job.DbBackJob', '0 0 0 1/1 * ? ', 1, 1, '2020-08-26 18:42:08');
+
+INSERT INTO `SYS_VER` VALUES (10, '3.2.0', '2020-10-31 16:43:00', 'zhanghc', '');
