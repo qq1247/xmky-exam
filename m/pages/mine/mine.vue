78cfc690909070bf774c1677d35c915a47004529
che <gtb791411878@163.com>
2022-08-04 17:19:17 +0800

    bug修复：移动端账号切换时姓名未变更

--- m/pages/mine/mine.vue
+++ m/pages/mine/mine.vue
@@ -7,39 +7,41 @@
 			<u-avatar v-if="userInfo.user" size="48" :src="`${baseURL}/${userInfo.user.headFileId}`"></u-avatar>
 			<view class="user-info">
 				<text class="user-name">
-					{{loginInfo.userName}}
+					{{ this.userName }}
 				</text>
 				<text class="user-level">
-					普通用户 
-				</text>	
+					普通用户
+				</text>
 			</view>
 			<u-icon name="arrow-right"></u-icon>
 		</navigator>
-		
+
 		<view class="report" v-if="userInfo.exam">
 			<view class="report-item">
 				<image src="@/static/my/index-paper.png"></image>
-				<text class="item-intro">参加：{{userInfo.exam.num}}</text>
+				<text class="item-intro">参加：{{ userInfo.exam.num }}</text>
 			</view>
 			<view class="report-item">
 				<image src="@/static/my/index-exam.png"></image>
-				<text class="item-intro">缺考：{{userInfo.exam.missNum}}</text>
+				<text class="item-intro">缺考：{{ userInfo.exam.missNum }}</text>
 			</view>
 			<view class="report-item">
 				<image src="@/static/my/index-question.png"></image>
-				<text class="item-intro">及格：{{userInfo.exam.succNum}}</text>
+				<text class="item-intro">及格：{{ userInfo.exam.succNum }}</text>
 			</view>
 			<view class="report-item">
 				<image src="@/static/my/index-mark.png"></image>
-				<text class="item-intro">参加：{{userInfo.exam.top}}</text>
+				<text class="item-intro">参加：{{ userInfo.exam.top }}</text>
 			</view>
 		</view>
-		
+
 		<div class="cell">
-			<u-cell icon="setting-fill" title="我的考试" :isLink="true" url="/pages/examList/examList" arrow-direction="right">
+			<u-cell icon="setting-fill" title="我的考试" :isLink="true" url="/pages/examList/examList"
+				arrow-direction="right">
 				<u-image slot="icon" src="@/static/my/index-exam.png" width="16" height="16"></u-image>
 			</u-cell>
-			<u-cell icon="setting-fill" title="我的模拟" :isLink="true" url="/pages/testList/testList" arrow-direction="right" :border="false">
+			<u-cell icon="setting-fill" title="我的模拟" :isLink="true" url="/pages/testList/testList"
+				arrow-direction="right" :border="false">
 				<u-image slot="icon" src="@/static/my/index-question.png" width="16" height="16"></u-image>
 			</u-cell>
 		</div>
@@ -47,67 +49,70 @@
 </template>
 
 <script>
-	import {
-		getUserInfo
-	} from '@/api/mine.js'
-	import {
-		setTitle
-	} from '@/common/setTitle'
-	export default {
-		data() {
-			return {
-				baseURL: getApp().globalData.baseUrl,
-				userInfo: {},
-				loginInfo: uni.getStorageSync('userInfo')
-			};
-		},
-		onShow() {
-			this.getUserInfo()
-		},
-		mounted() {
-			setTitle(getApp().globalData.entName)
-		},
-		methods: {
-			async getUserInfo() {
-				const userInfo = await getUserInfo()
-				this.userInfo = Object.keys(userInfo.data).length ?
-					userInfo.data :
-					{
-						user: {
-							id: null,
-							name: '',
-							type: null,
-							headFileId: null
-						},
-						org: {
-							id: null,
-							name: ''
-						},
-						exam: {
-							num: 0,
-							missNum: 0,
-							succNum: 0,
-							top: 0
-						},
-						score: {
-							avg: null,
-							min: null,
-							max: null,
-							sd: null
-						}
+import {
+	getUserInfo
+} from '@/api/mine.js'
+import {
+	setTitle
+} from '@/common/setTitle'
+export default {
+	data() {
+		return {
+			baseURL: getApp().globalData.baseUrl,
+			userInfo: {},
+			loginInfo: uni.getStorageSync('userInfo'),
+			userName: ""
+		};
+	},
+	onShow() {
+		this.getUserInfo()
+		this.userName = uni.getStorageSync('userInfo').userName
+	},
+	mounted() {
+		setTitle(getApp().globalData.entName)
+	},
+	methods: {
+		async getUserInfo() {
+			const userInfo = await getUserInfo()
+			this.userInfo = Object.keys(userInfo.data).length ?
+				userInfo.data :
+				{
+					user: {
+						id: null,
+						name: '',
+						type: null,
+						headFileId: null
+					},
+					org: {
+						id: null,
+						name: ''
+					},
+					exam: {
+						num: 0,
+						missNum: 0,
+						succNum: 0,
+						top: 0
+					},
+					score: {
+						avg: null,
+						min: null,
+						max: null,
+						sd: null
 					}
-			}
+				}
 		}
 	}
+}
 </script>
 
 <style lang="scss" scoped>
-.user{
+.user {
 	width: 100%;
 	display: flex;
 	padding: 10px 20px;
 	background: #fff;
 	box-sizing: border-box;
+
 	.user-info {
 		display: flex;
 		flex-direction: column;
@@ -115,10 +120,12 @@
 		margin-left: 20px;
 		flex: 1;
 	}
+
 	.user-name {
 		font-size: 32rpx;
 		color: #000;
 	}
+
 	.user-level {
 		font-size: 24rpx;
 		background: rgba(#0094e5, .6);
@@ -131,12 +138,13 @@
 	}
 }
 
-.report{
+.report {
 	width: 710rpx;
 	background: #fff;
 	border-radius: 10rpx;
 	margin: 16rpx auto;
 	display: flex;
+
 	.report-item {
 		flex: 1;
 		display: flex;
@@ -144,10 +152,12 @@
 		justify-content: center;
 		align-items: center;
 		padding: 40rpx 0;
+
 		uni-image {
 			width: 48rpx;
 			height: 48rpx;
 		}
+
 		.item-intro {
 			margin-top: 16rpx;
 			font-size: 26rpx;
@@ -160,6 +170,7 @@
 	background: #fff;
 	border-radius: 10rpx;
 	width: 720rpx;
+
 	/deep/ .u-cell__body {
 		padding: 30rpx;
 	}
