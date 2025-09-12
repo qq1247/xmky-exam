e153c64172f133e7d65e792630b70cabf82d06e2
qq1247 <1247qq@163.com>
2025-09-12 15:34:00 +0800

    优化改进：添加vconsole，方便移动端调试

--- m/main.js
+++ m/main.js
@@ -1,35 +1,39 @@
 // #ifndef VUE3
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import App from './App';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-App.mpType = 'app'
+App.mpType = 'app';
 
 const app = new Vue({
 	...App
-})
-app.$mount()
+});
+app.$mount();
 // #endif
 
 // #ifdef VUE3
-import {
-	createSSRApp
-} from 'vue'
+import { createSSRApp } from 'vue';
 import * as Pinia from 'pinia';
-import {
-	createUnistorage
-} from "./uni_modules/pinia-plugin-unistorage";
-import App from './App.vue'
+import { createUnistorage } from './uni_modules/pinia-plugin-unistorage';
+import App from './App.vue';
 export function createApp() {
-	const app = createSSRApp(App)
+	const app = createSSRApp(App);
 	const store = Pinia.createPinia();
 	store.use(createUnistorage());
-	app.use(store)
+	app.use(store);
 
 	return {
 		app,
-		Pinia,
-	}
+		Pinia
+	};
+}
+// #endif
+
+// #ifdef H5
+if (process.env.NODE_ENV === 'development') {
+  import('vconsole').then(({ default: VConsole }) => {
+    new VConsole()
+  })
 }
 // #endif
