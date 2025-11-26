dd9134537c5dae96f3b21f9aa59a9157f562ea28
qq1247 <1247qq@163.com>
2025-11-26 19:47:36 +0800

    bug修复：浏览器的记住账号密码功能，为了提示已填充设置了背景色，影响整体美观性（css强制覆盖）

--- h5/src/views/login/login.vue
+++ h5/src/views/login/login.vue
@@ -522,3 +522,15 @@ async function tempIn() {
     }
 }
 </style>
+<style>
+/* 浏览器的记住账号密码功能，为了提示已填充设置了背景色，影响整体美观性 */
+input:-webkit-autofill,
+input:-webkit-autofill:hover,
+input:-webkit-autofill:focus,
+input:-webkit-autofill:active {
+    -webkit-box-shadow: 0 0 0 30px #D2ECFF inset !important;
+    box-shadow: 0 0 0 30px #D2ECFF inset !important;
+    -webkit-text-fill-color: #606266 !important;
+    transition: background-color 5000s ease-in-out 0s;
+}
+</style>
